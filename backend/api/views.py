7637f0fb0998b5209ffb624c2c107c905310ed62
anand <anandmaurya@hotmail.com>
2024-04-26 15:35:55 +0100

    fixed timezone key

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,10 +4,12 @@ from django.utils import timezone
 def index(request):
     current_time = timezone.localtime().strftime("%-I:%S %p")
     current_date = timezone.localtime().strftime("%A %m %-Y")
+    zone = timezone.get_default_timezone_name()
 
     data = {
         'time': current_time,
         'date': current_date,
+        'zone': zone
     }
 
     return JsonResponse(data)
