e9e13be73adebd2f9b296aed63ca7dbe2c3346ce
anand <anandmaurya@hotmail.com>
2024-04-26 15:19:23 +0100

    removed unwanted styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,39 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [currentDate, setCurrentDate] = useState(0);
+
   useEffect(() => {
-    fetch(' http://0.0.0.0:8000/').then(res => res.json()).then(data => {
-        setCurrentTime(data.time);
-        setCurrentDate(data.date)
-      });
-    }, []);
+    const fetchData = () => {
+      fetch("http://0.0.0.0:8000/")
+        .then((res) => res.json())
+        .then((data) => {
+          setCurrentTime(data.time);
+          setCurrentDate(data.date);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    };
+    fetchData();
+    const intervalId = setInterval(fetchData, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
-      <p>The date is {currentDate} and the time is {currentTime}.</p>
+      <p>
+        The date is {currentDate} and the time is {currentTime}.
+      </p>
+      <br />
+      <p>
+        If you notice the time consistently updating, it signifies that the
+        connection to the backend API is operating correctly.
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
